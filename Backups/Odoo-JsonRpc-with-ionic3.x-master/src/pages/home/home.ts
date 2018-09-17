37077f5f511d67cc2cddfef2586fdf0012c1dce5
jdvillegas25 <soporteerp@itsoluciones.net>
2018-09-17 09:41:13 -0500

    Cambios encontrados

--- Backups/Odoo-JsonRpc-with-ionic3.x-master/src/pages/home/home.ts
+++ Backups/Odoo-JsonRpc-with-ionic3.x-master/src/pages/home/home.ts
@@ -20,9 +20,6 @@ export class HomePage {
     probability: number;
     partner_id: string;
     name: string;
-    colorDanger:boolean;
-    colorwarning:boolean;
-    colorSuccess:boolean;
   }> = [];
 
   private items: Array<{
@@ -30,21 +27,24 @@ export class HomePage {
     probability: number;
     partner_id: string;
     name: string;
-    colorDanger:boolean;
-    colorwarning:boolean;
-    colorSuccess:boolean;
   }> = [];
 
   private partner = "crm.lead";
 
-  constructor(private navCtrl: NavController, private odooRpc: OdooJsonRpc, private alertCtrl: AlertController, private network: Network, private alert: AlertController, private utils: Utils) {
+  constructor(
+    private navCtrl: NavController,
+    private odooRpc: OdooJsonRpc,
+    private alertCtrl: AlertController,
+    private network: Network,
+    private alert: AlertController,
+    private utils: Utils,
+  ) {
     this.display();
   }
 
   private display(): void {
-    let domain = [["user_id", "=", JSON.parse(localStorage.getItem('token'))['uid']]];
     this.odooRpc
-      .searchRead(this.partner, domain, [], 0, 0, "")
+      .searchRead(this.partner, [], [], 0, 0, "")
       .then((partner: any) => {
         this.fillParners(partner);
       });
@@ -54,15 +54,13 @@ export class HomePage {
     let json = JSON.parse(partners._body);
     if (!json.error) {
       let query = json["result"].records;
+
       for (let i in query) {
         this.partnerArray.push({
           id: query[i].id,
           probability: query[i].probability == false ? "N/A" : query[i].probability,
           name: query[i].name == false ? "N/A" : query[i].name,
-          partner_id: query[i].partner_id == false ? "N/A" : query[i].partner_id,
-          colorDanger:query[i].probability < 30 ? true : false,
-          colorwarning:query[i].probability >= 30 && query[i].probability < 70 ? true : false,
-          colorSuccess:query[i].probability > 70 ? true : false,
+          partner_id: query[i].partner_id == false ? "N/A" : query[i].partner_id
         });
       }
     }
@@ -104,15 +102,16 @@ export class HomePage {
   }
 
   private delete(idx: number) {
-    this.odooRpc.updateRecord(this.partner, this.partnerArray[idx].id,{active:false});
+    this.odooRpc.deleteRecord(this.partner, this.partnerArray[idx].id);
     this.utils.presentToast(
-      this.partnerArray[idx].name + " Se elimino con exito",
-      5000,
+      this.partnerArray[idx].name + " Deleted Successfully",
+      2000,
       true,
       "top"
     );
     this.partnerArray.splice(idx, 1);
   }
+
   viewProfile(): void {
     this.navCtrl.push(ProfilePage);
   }
