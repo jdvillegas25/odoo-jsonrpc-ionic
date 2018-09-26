415517cf0db1e11d153d2a0e1cf1aad845c03ca0
Desarrollo4 <desarrollo4@itsoluciones.net>
2018-09-26 16:15:55 -0500

    Se intenta hacer la insercion de la oportunidad pero por errores de tipo de datos no se deja insertar

--- Odoo-JsonRpc-with-ionic3.x-master/src/pages/form-probabilidad/form-probabilidad.ts
+++ Odoo-JsonRpc-with-ionic3.x-master/src/pages/form-probabilidad/form-probabilidad.ts
@@ -1,4 +1,5 @@
 import { OdooJsonRpc } from '../../services/odoojsonrpc';
+import { Utils } from '../../services/utils';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, LoadingController, Platform, ToastController } from 'ionic-angular';
 
@@ -20,6 +21,7 @@ export class FormProbabilidadPage {
   private website: any;
   private calificacion: any;
   private tags: any;
+  private city: any;
   private cliente: any;
   private dirContact: any;
   private nameContact: any;
@@ -27,6 +29,7 @@ export class FormProbabilidadPage {
   private movilContact: any;
   private emailContact: any;
   private notaInterna: any;
+  private titleAction: any;
   private listaClientes: any;
   private namePartner: any;
   private InfoVendedor: any = JSON.parse(localStorage.getItem('token'));
@@ -34,11 +37,13 @@ export class FormProbabilidadPage {
   private pest: string = "oportunidad";
   private isAndroid: boolean = false;
   private listaTags: any;
+  private listCity: any;
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private odooRpc: OdooJsonRpc, public loadingCtrl: LoadingController, platform: Platform, public toastCtrl: ToastController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private odooRpc: OdooJsonRpc, public loadingCtrl: LoadingController, platform: Platform, public toastCtrl: ToastController,private utils:Utils) {
     this.getClientes();
     this.getTags();
+    this.getCity();
     this.isAndroid = platform.is('android');
 
   }
@@ -70,10 +75,19 @@ export class FormProbabilidadPage {
       }
     });
   }
+  private getCity (){
+    let tableCity = "res.country.state"
+    this.odooRpc.searchRead(tableCity, [["country_id","=",50]], [], 0, 0, "").then((city: any) => {
+      let json = JSON.parse(city._body);
+      if (!json.error) {
+        this.listCity = json["result"].records;
+      }
+    });
+  }
   private saveData() {
     if (!this.name) {
       const toast = this.toastCtrl.create({
-        message: 'Agrege un nombre para la oportunidad.',
+        message: 'Por favor agrege un nombre para la oportunidad.',
         duration: 3000,
         showCloseButton: true,
         closeButtonText: "OK"
@@ -82,7 +96,7 @@ export class FormProbabilidadPage {
     }
     else if (!this.probabilidad) {
       const toast = this.toastCtrl.create({
-        message: 'Seleccione un rango de probabilidad',
+        message: 'Por favor seleccione un rango de probabilidad',
         duration: 3000,
         showCloseButton: true,
         closeButtonText: "OK"
@@ -91,7 +105,7 @@ export class FormProbabilidadPage {
     }
     else if (!this.cliente) {
       const toast = this.toastCtrl.create({
-        message: 'Seleccione un cliente',
+        message: 'Por favor seleccione un cliente',
         duration: 3000,
         showCloseButton: true,
         closeButtonText: "OK"
@@ -100,7 +114,7 @@ export class FormProbabilidadPage {
     }
     else if (!this.email) {
       const toast = this.toastCtrl.create({
-        message: 'Agregue un correo',
+        message: 'Por favor agregue un correo para el cliente',
         duration: 3000,
         showCloseButton: true,
         closeButtonText: "OK"
@@ -109,7 +123,7 @@ export class FormProbabilidadPage {
     }
     else if (!this.telefono) {
       const toast = this.toastCtrl.create({
-        message: 'Agregue un teléfono',
+        message: 'Por favor agregue un teléfono para el cliente',
         duration: 3000,
         showCloseButton: true,
         closeButtonText: "OK"
@@ -118,7 +132,7 @@ export class FormProbabilidadPage {
     }
     else if (this.nextAcitivity && !this.fechaActividad) {
       const toast = this.toastCtrl.create({
-        message: 'Seleccione una fecha para la proxima actividad',
+        message: 'Por favor seleccione una fecha para la proxima actividad',
         duration: 3000,
         showCloseButton: true,
         closeButtonText: "OK"
@@ -127,7 +141,7 @@ export class FormProbabilidadPage {
     }
     else if (!this.nameContact) {
       const toast = this.toastCtrl.create({
-        message: 'Agregue un nombre de contacto',
+        message: 'Por favor agregue un nombre de contacto',
         duration: 3000,
         showCloseButton: true,
         closeButtonText: "OK"
@@ -136,7 +150,7 @@ export class FormProbabilidadPage {
     }
     else if (!this.movilContact) {
       const toast = this.toastCtrl.create({
-        message: 'Agregue un telefono de contacto',
+        message: 'Por favor agregue un telefono de contacto',
         duration: 3000,
         showCloseButton: true,
         closeButtonText: "OK"
@@ -145,7 +159,7 @@ export class FormProbabilidadPage {
     }
     else if (!this.emailContact) {
       const toast = this.toastCtrl.create({
-        message: 'Agregue un email de contacto',
+        message: 'Por favor agregue un email de contacto',
         duration: 3000,
         showCloseButton: true,
         closeButtonText: "OK"
@@ -153,42 +167,49 @@ export class FormProbabilidadPage {
       toast.present();
     }
     else {
-      let dataInsert = {
+      let params = {
         date_closed: this.cierrePrevisto,
         probability: this.probabilidad,
         country_id: 50,
         write_uid: JSON.parse(localStorage.getItem('token'))['uid'],
         contact_name: this.nameContact,
         partner_id: this.cliente,
-        // partner_name: this.,
-        // company_id: this.,
-        // priority: this.,
-        // next_activity_id: this.,
-        // type: this.,
-        // function: this.,
-        // description: this.,
-        // create_uid: this.,
-        // date_deadline: this.,
-        // title_action: this.,
-        // phone: this.,
-        // user_id: this.,
-        // date_action: this.,
-        // name: this.name,
-        // stage_id: this.,
-        // mobile: this.,
-        // street: this.,
-        // state_id: this.
+        partner_name: this.namePartner,
+        company_id: 1,
+        next_activity_id: this.nextAcitivity,
+        type: "opportunity",
+        function: this.functionContact,
+        description: (!this.notaInterna)?'':this.notaInterna,
+        create_uid: JSON.parse(localStorage.getItem('token'))['uid'],
+        title_action: this.titleAction,
+        phone: this.movilContact,
+        user_id: JSON.parse(localStorage.getItem('token'))['uid'],
+        date_action: this.fechaActividad,
+        name: this.name,
+        stage_id: 1,
+        mobile: this.telefono,
+        street: this.dirContact,
+        state_id: this.city[0],
+        city: this.city[1],
       };
+      console.log(params); 
+      let model = "crm.lead";
+      this.odooRpc.createRecord(model,params).then((res:any)=>{
+        console.log(res);
+      }).catch((err:any)=>{
+        alert(err)
+      })
     }
   }
-  public persistCliente(cli) {
+  public persistCliente() {
     for (let client of this.listaClientes) {
-      if (cli == client.id) {
+      if (this.cliente == client.id) {
         this.email = (client.email == false)?'':client.email;
         this.telefono = client.phone;
         this.mobil = client.mobil;
         this.namePartner = client.name;
         this.dirContact = client.street;
+        this.city = client.state_id;
       }
     }
   }
